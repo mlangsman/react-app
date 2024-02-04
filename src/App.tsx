7b74b91cd35383e88fdb03932a5ce84e62a11a4b
Marc Langsman <marc@langsman.com>
2024-02-04 08:01:50 +0000

    Created ListGroup css and refactored into a folder

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-//import ListGroup from "./components/ListGroup";
+import ListGroup from "./components/ListGroup";
 import { useState } from "react";
 import Alert from "./components/Alert";
 import Button, { ButtonType } from "./components/Button";
@@ -20,13 +20,19 @@ function App() {
       />
     </div>
   );
-}*/
+}
+*/
 
 function App() {
   const [showAlert, setShowAlert] = useState(false);
   const handleClick = (buttonType: ButtonType) => {
     setShowAlert(true);
     console.log(buttonType);
+  };
+  const items = ["New York", "London", "San Francisco", "Paris"];
+
+  const handleSelectItem = (item: string) => {
+    console.log(item);
   };
 
   return (
@@ -35,6 +41,12 @@ function App() {
       <Button buttonType={ButtonType.Primary} onButtonClick={handleClick}>
         Primary
       </Button>
+
+      <ListGroup
+        items={items}
+        heading="Cities"
+        onSelectedItem={handleSelectItem}
+      />
     </div>
   );
 }
